a186e5a4a178ce8d25acbc12afb5597cc17ca5f5
Rodrigo <rodrigo@vancouver4us.com>
2025-09-26 16:30:13 -0700

    url parameters

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import http from 'http';
+import url from 'url';
 import dt from './modules/datetime.js';
 
+const nameParam = "name";
+
 http.createServer((req, res) => {
+    let urlParams = url.parse(req.url, true);
+
     res.writeHead(200, {'Content-Type': 'text/html'});
     
-    res.write(`Now: ${dt.date()}`);
+    res.write('<p style="color: blue;">');
+    res.write(`Hello ${urlParams.query[nameParam]}!`);
+    res.write('<br/>')
+    res.write(`Server time: ${dt.date()}`);
+    res.write('</p>');
 
     res.end();
 }).listen(8889);
